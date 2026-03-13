925e228ad335784fc292af09cf277a3ec31c3c91
Eldar Shaidullin <apps@exx.ee>
2026-03-14 00:08:15 +0300

    Initial Commit

---
+++ TempusFugit/TempusFugitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempusFugitApp.swift
+//  TempusFugit
+//
+//  Created by es on 2026-03-14.
+//
+
+import SwiftUI
+
+@main
+struct TempusFugitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
